1dd412b8db35d595201acebb5401e8e865c2cef1
stefanrogic <Stefanrogic@protonmail.com>
2025-07-08 10:54:06 +0200

    Code refactoring

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean exit = false;
-
-        Scanner scanner = new Scanner(System.in);
         Task.loadTasks("tasks.json");
         Task.printTasks();
+
+        Scanner scanner = new Scanner(System.in);
         System.out.println("\nType 'help' for list of commands");
 
+        boolean exit = false;
         while (!exit) {
             System.out.print("task-cli ");
             String command = scanner.nextLine().trim();
             TaskManager.manage(command);
         }
-        scanner.close();
+
     }
 }
